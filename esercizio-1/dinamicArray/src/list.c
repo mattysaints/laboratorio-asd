21f6d37844c1fe2ac7f9ca12585f51459e782143
Davide Zago <davide.zago@edu.unito.it>
2019-03-26 22:08:48 +0100

    Completamento funzioni create, empty e insert

--- esercizio-1/dinamicArray/src/list.c
+++ esercizio-1/dinamicArray/src/list.c
@@ -1,4 +1,7 @@
+#import "list.h"
+
 #import <stddef.h>
+#import <stdlib.h>
 
 struct _List {
   void** array;
@@ -6,10 +9,10 @@ struct _List {
   size_t length;
 };
 
-List *List_create(size_t bytes) {
+List *List_create(size_t length) {
   if(bytes>0) {
     List *result = (List *)malloc(sizeof(List));
-    result->array = (void**)malloc(sizeof(void*)*bytes);
+    result->array = (void**)malloc(sizeof(void*)*length);
     result->size = 0;
     result->length = 1;
     return result;
@@ -21,10 +24,13 @@ int List_empty(List *l) {
     return l->size==0;
 }
 
-void List_insert(List *l) {
+void List_insert(List *l, void *elem) {
   if(l) {
-    if(l->size>=l->length) {
-
+    if(l->size >= l->length) {
+      l->length *= 2;
+      l->array = (void**)realloc(l->array,sizeof(void*)*l->length);
     }
+    l->array[l->size] = elem;
+    l->size++;
   }
 }
--- esercizio-1/dinamicArray/src/list.h
+++ esercizio-1/dinamicArray/src/list.h
@@ -1,1 +1,16 @@
+#pragma once
+
+// Definition of the opaque type
 typedef struct _List List;
+
+/*
+   Returns a new allocated list
+   length: given initial length of the list
+ */
+List *List_create(size_t length);
+
+// Returns 0 if the argument type of list is empty, 1 otherwise
+int List_empty(List *l);
+
+// Inserts an element to the list at the end of the queue
+void List_insert(List *l, void *elem);
