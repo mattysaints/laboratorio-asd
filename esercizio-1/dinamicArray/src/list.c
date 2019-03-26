6a6f5e9f7795ae6bd19a6a3d43e313854718064c
Davide Zago <davide.zago@edu.unito.it>
2019-03-26 13:04:02 +0100

    Aggiunta di funzioni sulle liste. Da completare

--- esercizio-1/dinamicArray/src/list.c
+++ esercizio-1/dinamicArray/src/list.c
@@ -1,3 +1,30 @@
+#import <stddef.h>
+
 struct _List {
-  
+  void** array;
+  size_t size;
+  size_t length;
 };
+
+List *List_create(size_t bytes) {
+  if(bytes>0) {
+    List *result = (List *)malloc(sizeof(List));
+    result->array = (void**)malloc(sizeof(void*)*bytes);
+    result->size = 0;
+    result->length = 1;
+    return result;
+  }
+}
+
+int List_empty(List *l) {
+  if(l)
+    return l->size==0;
+}
+
+void List_insert(List *l) {
+  if(l) {
+    if(l->size>=l->length) {
+
+    }
+  }
+}
