605443f9411eacfffa32f3037d72cb2c028f175c
Davide Zago <davide.zago@edu.unito.it>
2019-03-29 19:22:46 +0100

    Aggiunta delle funzioni di test che mancavano

--- esercizio-1/dinamicArray/src/list_tests.c
+++ esercizio-1/dinamicArray/src/list_tests.c
@@ -17,9 +17,9 @@ static int *new_int(int num) {
 static List *build_list() {
   List *l = List_create(4);
 
-  List_insert(l,new_int(3));
-  List_insert(l,new_int(8));
-  List_insert(l,new_int(2));
+  List_add(l,new_int(3));
+  List_add(l,new_int(8));
+  List_add(l,new_int(2));
   return l;
 }
 
@@ -27,18 +27,81 @@ static void test_list_new_not_null() {
   List *l = build_list();
 
   TEST_ASSERT_NOT_NULL(l);
+  List_destroy(l);
+}
+
+static void test_list_destroy() {
+  List *l = build_list();
+  List_destroy(l);
+
+  TEST_ASSERT_EQUAL_INT(1,1);
 }
 
 static void test_list_empty() {
   List *l = List_create(7);
 
   TEST_ASSERT_EQUAL_INT(1,List_empty(l));
+  List_destroy(l);
 }
 
+static void test_list_size() {
+  List *l = build_list();
+
+  TEST_ASSERT_EQUAL_INT(3,List_size(l));
+  List_destroy(l);
+}
+
+static void test_list_add() {
+  List *l = List_create(5);
+
+  List_add(l,new_int(4));
+  TEST_ASSERT_EQUAL_INT(1,List_size(l));
+  TEST_ASSERT_EQUAL_INT(4,*(int *)List_get(l,0));
+  List_destroy(l);
+}
+
+static void test_list_add_index() {
+  List *l = build_list();
+
+  List_add_index(l,new_int(10),1);
+  TEST_ASSERT_EQUAL_INT(4,List_size(l));
+  TEST_ASSERT_EQUAL_INT(10,*(int *)List_get(l,1));
+  List_destroy(l);
+}
+
+static void test_list_del() {
+  List *l = build_list();
+
+  List_del(l);
+  TEST_ASSERT_EQUAL_INT(2,List_size(l));
+  List_destroy(l);
+}
+
+static void test_list_del_index() {
+  List *l = build_list();
+
+  List_del_index(l,1);
+  TEST_ASSERT_EQUAL_INT(2,List_size(l));
+  TEST_ASSERT_EQUAL_INT(2,*(int *)List_get(l,1));
+  List_destroy(l);
+}
+
+static void test_list_get() {
+  List *l = build_list();
+
+  TEST_ASSERT_EQUAL_INT(3,*(int *)List_get(l,0));
+}
 
 int main() {
   UNITY_BEGIN();
   RUN_TEST(test_list_new_not_null);
+  RUN_TEST(test_list_destroy);
   RUN_TEST(test_list_empty);
+  RUN_TEST(test_list_size);
+  RUN_TEST(test_list_add);
+  RUN_TEST(test_list_add_index);
+  RUN_TEST(test_list_del);
+  RUN_TEST(test_list_del_index);
+  RUN_TEST(test_list_get);
   UNITY_END();
 }
