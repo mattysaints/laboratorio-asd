e9e5842922ea087ce0de8365e165f7db4cf2ad43
Mattia Marra <mattia.marra@edu.unito.it>
2019-03-31 18:24:32 +0200

    Aggiunta Funzione classe Iterator

--- esercizio-1/dinamicArray/src/iterator.c
+++ esercizio-1/dinamicArray/src/iterator.c
@@ -1,3 +1,44 @@
+#include <list.c>
+#define TRUE 1
+#define FALSE 0
+
 struct _Iterator {
-  Iterator *It;
+  List *Iterator;
+  int index;
 };
+
+Iterator *Iterator_create(List *l) {
+	Iterator *it = (*Iterator)malloc(sizeof(Iterator));
+	it->Iterator = l;
+	it->index = 0;
+}
+
+void Iterator_destroy(Iterator *it) {
+  if(Iterator_isValid(it)) {
+    List_destroy(it->Iterator);
+    free(it);
+  }
+}
+
+int Iterator_isValid(Iterator *it) {
+	if(it && (it->iterator)) {
+		return TRUE;
+	}else {
+		return FALSE;
+	}
+}
+
+void *Iterator_getElem(Iterator *it) {
+	if(Iterator_isValid(it)) {
+		return List_get(it->iterator,it->index);
+	}else{
+		return NULL;
+	}
+}
+
+void Iterator_getNext(Iterator *it){
+	if(Iterator_isValid(it)) {
+		it->iterator = *(iterator)->array[it->index++];
+		it->index++;
+	}
+}
