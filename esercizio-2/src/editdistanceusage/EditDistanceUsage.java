6557c72898f99adfcddbda976f1594b5371baaa6
Davide Zago <davide.zago@edu.unito.it>
2019-04-10 14:22:46 +0200

    Modifiche al file usage. Non ultimato

--- esercizio-2/src/editdistanceusage/EditDistanceUsage.java
+++ esercizio-2/src/editdistanceusage/EditDistanceUsage.java
@@ -1,19 +1,21 @@
 package editdistanceusage;
 
-import java.io.Reader;
+import java.io.FileReader;
+import java.io.BufferedReader;
+import java.io.IOException;
 import editdistance.EditDistance;
 
 public class EditDistanceUsage {
 
-  private static String[] getWords(String filepath) {
+  private static String[] getWords(String filepath) throws IOException {
     if(filepath == null)
       throw new IllegalArgumentException("Filepath cannot be null");
 
-    BufferedReader br = new FileReader(filepath);
+    BufferedReader br = new BufferedReader(new FileReader(filepath));
     char peek;
     String content = "";
     
-    while((peek=br.read()) != (char)-1)
+    while((peek=(char)br.read()) != (char)-1)
       content += peek;
     String[] result = content.split("\\W");
     br.close();
@@ -21,7 +23,12 @@ public class EditDistanceUsage {
   } // getWords
 
   public static void main(String[] args) {
-
+    try {
+      String[] dict = getWords("../resources/dictionary.txt");
+      String[] file = getWords("../resources/correctme.txt");
+    } catch(IOException e) {
+      e.printStackTrace();
+    }
   } // main
 
 } // class
