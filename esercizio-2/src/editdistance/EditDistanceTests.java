259909f57ad0bef917e54abdbbe7187fb111efdc
Mattia Marra <mattia.marra@edu.unito.it>
2019-04-13 17:56:14 +0200

    Aggiunta Funzioni Test EditDistance

--- esercizio-2/src/editdistance/EditDistanceTests.java
+++ esercizio-2/src/editdistance/EditDistanceTests.java
@@ -1,5 +1,8 @@
 package editdistance;
 
+import static org.junit.Assert.*;
+import org.junit.Test;
+
 public class EditDistanceTests {
   
   @Test
@@ -12,13 +15,40 @@ public class EditDistanceTests {
   
   @Test
   public void EditDyn_is_correct(){
-	  String s1 = "caco";
-	  String s2 = "cane";
+	  String s1 = "acqua";
+	  String s2 = "accua";
 	  
 	  assertTrue(2 == editDistanceDyn(s1,s2));
   }
-  
-  
-  
-  
+
+  @Test
+  public void Same_String(){
+  	String s1 = "pablo";
+	  String s2 = "pablo";
+	  
+	  assertTrue(0 == editDistanceDyn(s1,s2));
+  }
+
+  @Test
+  public void Same_String(){
+  	String s1 = "";
+	  String s2 = "";
+	  
+	  assertEquals(s1,s2);
+  }
+
+  @Test
+  public void is_notNull(){
+  	String s1 = "pablo";
+	  
+	  assertNotNull(s1);
+  }
+
+  @Test
+  public void are_Null(){
+  	String s1;
+  	String s2;
+	  
+	  assertNull(s1,s2);
+  }  
 } // class
