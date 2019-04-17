61b05d500dc77ab9a152c21651f2f396a24aeb3f
Davide Zago <davide.zago@edu.unito.it>
2019-04-18 00:26:22 +0200

    Aggiunta del comparator per ordinare le stringhe per lunghezza, utile per un esperimento nell'algoritmo che sfrutta la funzione editDistanceDyn

---
+++ esercizio-2/src/editdistanceusage/LengthOrder_String.java
@@ -0,0 +1,24 @@
+package editdistanceusage;
+
+import java.util.Comparator;
+
+/**
+ * String comparator
+ */
+public class LengthOrder_String implements Comparator<String> {
+
+  /**
+   * Override of method compare: ordered by  length (ascending), then alphabetic
+   * @param s1: first string to compare
+   * @param s2: second string to compare
+   */
+  @Override
+  public int compare(String s1, String s2) {
+    if(s1.length() < s2.length())
+      return -1;
+    else if(s1.length() > s2.length())
+      return 1;
+    else
+      return s1.compareTo(s2);
+  }
+}
