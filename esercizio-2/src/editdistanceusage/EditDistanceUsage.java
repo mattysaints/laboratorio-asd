98c6ed2a0065acfb56e3de368cc834eee506d066
Davide Zago <davide.zago@edu.unito.it>
2019-04-18 00:27:30 +0200

    Aggiunta di varie alternative per il 'correttore automatico'. Ancora da scegliere la migliore e riordinare il codice

--- esercizio-2/src/editdistanceusage/EditDistanceUsage.java
+++ esercizio-2/src/editdistanceusage/EditDistanceUsage.java
@@ -5,7 +5,8 @@ import java.io.FileReader;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.lang.Math;
-import java.util.*;
+import java.util.Arrays;
+import java.util.ArrayList;
 
 public class EditDistanceUsage {
 
@@ -15,79 +16,166 @@ public class EditDistanceUsage {
 
     BufferedReader br = new BufferedReader(new FileReader(filepath));
     String line = null, string = "";
-    int i=0;
-    while((line = br.readLine())!=null && i<100000) {
+    while((line = br.readLine())!=null)
       string = string + line + "\n";
-      i++;
-    }
     br.close();
     return string.split("\\W");
   } // getWords
 
-  public static void exec() throws IOException {
+  private static ArrayList<String> getWordList(String filepath) throws IOException {
+    if(filepath == null)
+      throw new IllegalArgumentException("Filepath cannot be null");
+
+    ArrayList<String> res = new ArrayList<>();
+    BufferedReader br = new BufferedReader(new FileReader(filepath));
+    String string = "";
+    char peek = ' ';
+    boolean accumulate = false;
+    while((peek=(char)br.read())!=(char)-1) {
+      if(peek>='a' && peek<='z' || peek>='A' && peek<='Z') {
+        string += peek;
+        accumulate = true;
+      } else if(accumulate) {
+        res.add(string);
+        string = "";
+        accumulate = false;
+      }
+    }
+    br.close();
+    return res;
+  } // getWordList
+
+  private static String[] getWords2(String filepath) throws IOException {
+    ArrayList<String> tmp = getWordList(filepath);
+    String[] res = new String[tmp.size()];
+    for(int i=0; i<res.length; i++)
+      res[i] = tmp.get(i);
+    return res;
+  } // getWords2
+
+  private static void exec() throws IOException {
     String[] file = getWords("editdistanceusage/resources/correctme.txt");
-      String[] dict = getWords("editdistanceusage/resources/dictionary.txt");
 
-      for(int i=0; i<file.length; i++) {
-        if(file[i].length()==0)
-          continue;
-
-        int min = EditDistance.editDistanceDyn(file[i],dict[0]);
-        ArrayList<String> simWords = new ArrayList<>();
-        simWords.add(dict[0]);
-        for(int j=1; j<dict.length; j++) {
-          int ed = EditDistance.editDistanceDyn(file[i],dict[j]);
-          if(ed < min) {
-            simWords.clear();
-            simWords.add(dict[j]);
-          } else if(ed==min)
-            simWords.add(dict[j]);
-        }
-        System.out.println(file[i]+": "+simWords.toString()+"\n");
+    for(int i=0; i<file.length; i++) {
+      if(file[i].length()==0)
+        continue;
+      BufferedReader br = new BufferedReader(new FileReader("editdistanceusage/resources/dictionary.txt"));
+      String dict = br.readLine();
+      int min = EditDistance.editDistanceDyn(file[i],dict);
+      ArrayList<String> simWords = new ArrayList<>();
+      simWords.add(dict);
+      while((dict=br.readLine())!=null) {
+        int ed = EditDistance.editDistanceDyn(file[i],dict);
+        if(ed < min) {
+          min = ed;
+          simWords.clear();
+          simWords.add(dict);
+        } else if(ed==min)
+          simWords.add(dict);
       }
+      System.out.println(file[i]+": "+simWords+"\n");
+    }
   }
 
-  private static String readWord(BufferedReader br) throws IOException, IllegalArgumentException {
-    if(br!=null) {
-      char peek = ' ';
-      while(!((peek=(char)br.read())>='a' && peek <= 'z' || peek>='A' && peek <='Z'));
-      String word = ""+peek;
-      while((peek=(char)br.read())>='a' && peek <= 'z' || peek>='A' && peek <='Z')
-        word+=peek;
-      return word;
-    } else
-      throw new IllegalArgumentException("Parameter type of BufferedReader is null");
+  private static void exec2() throws IOException {
+    ArrayList<String> file = getWordList("editdistanceusage/resources/correctme.txt");
+    BufferedReader br = new BufferedReader(new FileReader("editdistanceusage/resources/dictionary.txt"));
+    int[] min_ed = new int[file.size()];
+    int[] ed = new int[file.size()];
+    
+    ArrayList<ArrayList<String>> simWords = new ArrayList<>();
+    for(int i=0; i<file.size(); i++)
+      simWords.add(new ArrayList<>());
+
+    //aggiunta del primo
+    String dict = br.readLine();
+    for(int i=0; i<file.size(); i++) {
+      min_ed[i] = EditDistance.editDistanceDyn(file.get(i),dict);
+      ed[i] = EditDistance.editDistanceDyn(file.get(i),dict);
+      simWords.get(i).add(dict);
+    }
+
+    //lettura file e aggiunta liste parole simili
+    while((dict=br.readLine())!=null) {
+      for(int i=0; i<file.size(); i++) {
+        ed[i] = EditDistance.editDistanceDyn(file.get(i),dict);
+        if(ed[i] < min_ed[i]) {
+          simWords.get(i).clear();
+          simWords.get(i).add(dict);
+          min_ed[i] = ed[i];
+        } else if(ed[i]==min_ed[i])
+          simWords.get(i).add(dict);
+      }
+    }
+  
+    //stampa delle liste
+    for(int i=0; i<file.size(); i++)
+      System.out.println(file.get(i)+": "+simWords.get(i)+"\n");
   }
 
-  public static void exec2() throws IOException, IllegalArgumentException {
-    BufferedReader br1 = new BufferedReader(new FileReader("editdistanceusage/resources/correctme.txt"));
+  // file e dizionario memorizzati in un array
+  private static void exec3() throws IOException {
+    String[] file = getWords("editdistanceusage/resources/correctme.txt");
+    Object[] dict = getWordList("editdistanceusage/resources/dictionary.txt").toArray();
 
-      String s1;
-      while((s1=readWord(br1))!="") {
-        BufferedReader br2 = new BufferedReader(new FileReader("editdistanceusage/resources/dictionary.txt"));
-        String s2 = readWord(br2);
-        int min = EditDistance.editDistanceDyn(s1,s2);
-        ArrayList<String> simWords = new ArrayList<>();
-        simWords.add(s2);
-        while((s2=readWord(br2))!="") {
-          int ed = EditDistance.editDistanceDyn(s1,s2);
-          if(ed < min) {
-            simWords.clear();
-            simWords.add(s2);
-          } else if(ed==min)
-            simWords.add(s2);
-        }
-        System.out.println(s1+": "+simWords.toString()+"\n");
+    for(int i=0; i<file.length; i++) {
+      if(file[i].length()==0)
+        continue;
+
+      int min = EditDistance.editDistanceDyn(file[i],(String)dict[0]);
+      ArrayList<String> simWords = new ArrayList<>();
+      simWords.add((String)dict[0]);
+      for(int j=1; j<dict.length; j++) {
+        int ed = EditDistance.editDistanceDyn(file[i],(String)dict[j]);
+        if(ed < min) {
+          min = ed;
+          simWords.clear();
+          simWords.add((String)dict[j]);
+        } else if(ed==min)
+          simWords.add((String)dict[j]);
       }
+      System.out.println(file[i]+": "+simWords+"\n");
+    }
+  }
+
+  // Dizionario in un array ordinato per lunghezza
+  private static void exec4() throws IOException {
+    String[] file = getWords("editdistanceusage/resources/correctme.txt");
+    String[] dict = getWords2("editdistanceusage/resources/dictionary.txt");
+    Arrays.sort(dict, new LengthOrder_String());
+
+    for(int i=0; i<file.length; i++) {
+      if(file[i].length()==0)
+        continue;
+
+      int min_ed = EditDistance.editDistanceDyn(file[i],(String)dict[0]);
+      int max_ed = min_ed;
+      ArrayList<String> simWords = new ArrayList<>();
+      simWords.add(dict[0]);
+      for(int j=1; j<dict.length; j++) {
+        int ed = EditDistance.editDistanceDyn(file[i],dict[j]);
+        max_ed = Math.max(max_ed,ed);
+        if(ed < min_ed) {
+          min_ed = ed;
+          simWords.clear();
+          simWords.add(dict[j]);
+        } else if(ed==min_ed)
+          simWords.add(dict[j]);
+        else if(dict[j].length()-file[i].length()>ed)
+          break;
+      }
+      System.out.println(file[i]+": "+simWords+"\n");
+    }
   }
 
   public static void main(String[] args) {
+    long begin = System.nanoTime();
     try {
-      exec();
-      //exec2();
+      exec4();  
     } catch(IOException e) {
       e.printStackTrace();
     }
+    System.out.println("Time: "+(System.nanoTime()-begin)/1000000000.0);
   } // main
 
 } // class
