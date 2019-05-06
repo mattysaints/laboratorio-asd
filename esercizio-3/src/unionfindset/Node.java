cb17a0f529e196126ee1884f3ec4fe9d3edbe408
Davide Zago <davide.zago@edu.unito.it>
2019-05-06 18:29:30 +0200

    Aggiunta possibili implementazioni dei metodi dell'es 3

--- esercizio-3/src/unionfindset/Node.java
+++ esercizio-3/src/unionfindset/Node.java
@@ -2,17 +2,23 @@ package unionfindset;
 
 public class Node<T> {
 
-  private T elem;
-  private Node<T> sup;
+  private T key;
+  private int rank;
+  private Node<T> parent;
 
-  private Node(T elem, Node<T> sup) {
-    this.elem = elem;
-    this.sup = sup;
+  private Node(T key) {
+    this.key = key;
+    this.rank = 0;
+    this.parent = this;
   }
 
-  public T getElem() { return elem; }
+  public T key() { return key; }
 
-  public Node<T> getSup() { return sup; }
+  public int rank() { return rank; }
 
-  public Node<T> setSup(Node<T> sup) { this.sup=sup; }
+  public Node<T> parent() { return parent; }
+
+  public Node<T> setParent(Node<T> parent) { this.parent=parent; }
+
+  public void rankUp() { this.rank++; }
 }
--- esercizio-3/src/unionfindset/UnionFindSet.java
+++ esercizio-3/src/unionfindset/UnionFindSet.java
@@ -4,33 +4,52 @@ import java.util.ArrayList;
 
 public class UnionFindSet<T> {
 
-  private ArrayList<Node<T>> forest;
+  private Node<T>[] forest;
 
   public UnionFindSet() { forest=null; }
 
-  // idea di implementazione
-  public void makeSet(T[] set) throws IllegalArgumentException {
+  public void makeSet(T[] set) throws IllegalArgumentException, UnionFindSetException {
     if(forest!=null)
       throw new UnionFindSetException("This set already exists");
     else if(set==null || set.length<=0)
       throw new IllegalArgumentException("The pararameter type T[] (array) must be not null and not empty");
     else {
-      forest = new ArrayList<>(set.length);
-      for(int i=0; i<set.length; i++) {
-        Node<T> temp = new Node<>(set[i]);
-        forest.add(temp,temp);
-      }
+      forest = new Node<>[set.length];
+      for(int i=0; i<set.length; i++)
+        forest[i] = new Node<>(set[i]);
     }
   }
 
   public void union(T x, T y) throws IllegalArgumentException, UnionFindSetException {
     if(x!=null && y!=null) {
-      int hx = 0, hy = 0;
+      Node<T> xNode = findNode(x);
+      Node<T> yNode = findNode(y);
+      if(xNode.rank()>yNode.rank())
+        yNode.setParent(xNode);
+      else if(xNode.rank()==yNode.rank()) {
+        xNode.setParent(yNode);
+        xNode.rankUp();
+      } else // xNode.rank() < yNode.rank()
+        xNode.setParent(yNode);
     } else
       throw new IllegalArgumentException("The pararameters type T must be not null");
   }
 
-  public UnionFindSet<T> find(T x) {
-      
+  private Node<T> findNode(T x) throws UnionFindSetException {
+    for(Node<T> node: forest)
+      if(node.equals(x))
+        return node;
+    throw new UnionFindSetException("Element not found in this set");
+  }
+
+  public Node<T> find(T x) throws IllegalArgumentException, UnionFindSetException {
+    if(x==null)
+      throw new IllegalArgumentException("The pararameter type T must be not null");
+    else {
+      Node<T> node = findNode(x);
+      if(node.parent()!=node)
+        node.setParent(find(node.parent()));
+      return node;
+    }
   }
 }
