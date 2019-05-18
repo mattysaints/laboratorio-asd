baaea9b3d5bc61931021a42ac1c0db3d52051b61
Mattia Marra <mattia.marra@edu.unito.it>
2019-05-18 17:08:11 +0200

    Modifica Test UnionFindSet

--- esercizio-3/src/unionfindset/UnionFindSetTests.java
+++ esercizio-3/src/unionfindset/UnionFindSetTests.java
@@ -6,15 +6,23 @@ import org.junit.Test;
 
 public class UnionFindSetTests {
 
-	private UnionFindSet<Integer> u1;
+	private UnionFindSet<Integer> u1,u2,u3;
 	private Integer i1, i2, i3;
+	private String s1,s2,s3;
 
 	@Before
 	public void init_UnionFindSet() {
 		u1 = new UnionFindSet<>();
+		u2 = new UnionFindSet<>();
+		u3 = new UnionFindSet<>();
+		
 		i1 = 3;
 		i2 = 4;
 		i3 = 2;
+
+		s1 = "a";
+		s2 = "c";
+		s3 = "e";
 	}
 
 	@Test
@@ -24,6 +32,27 @@ public class UnionFindSetTests {
 
 		for(Integer i: set)
 			assertEquals(i,u1.find(i));
+	}
+
+	@Test
+	public void test_makeSet_str() throws Exception {
+		String[] set = {s1,s2,s3};
+		u1.makeSet(set);
+
+		for(String i: set)
+			assertEquals(i,u1.find(i));
+	}
+
+	@Test 
+	public void test_unionSet_int() {
+		Integer[] set1 = {i1};
+		Integer[] set2 = {i2};
+
+		u1.makeSet(set1);
+		u2.makeSet(set2);
+
+		//union
+
 	}
 
 }
