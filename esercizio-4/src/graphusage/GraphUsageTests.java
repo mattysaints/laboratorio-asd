f1d997bbeaa230226735edddcf4bfba3cc15c867
Mattia Marra <mattia.marra@edu.unito.it>
2019-06-09 12:18:47 +0200

    Modifica Test GraphUsage

--- esercizio-4/src/graphusage/GraphUsageTests.java
+++ esercizio-4/src/graphusage/GraphUsageTests.java
@@ -4,27 +4,33 @@ import static org.junit.Assert.*;
 import org.junit.Before;
 import org.junit.Test;
 import graph.Graph;
+import graphusage.GraphUsage;
 import graph.GraphException;
 import graph.Arch;
 
 public class GraphUsageTests {
 
-  private Graph<Integer,Integer> grNotOr;
+  private Graph<String,Double> grNotOr;
 
 	@Before
-  public void createGraph() throws Exception{
+  public void createGraph() throws Exception {
     grNotOr = new Graph<>(false);
-    grNotOr.add(0); //A
-    grNotOr.add(1); //B
-    grNotOr.add(2); //C
-    grNotOr.add(3); //D
+    grNotOr.add("A"); //A
+    grNotOr.add("B"); //B
+    grNotOr.add("C"); //C
+    grNotOr.add("D"); //D
 
-    grNotOr.link(0,1,1);
-    grNotOr.link(1,2,2);
-    grNotOr.link(0,2,3);
-    grNotOr.link(0,3,5);
-    grNotOr.link(1,3,3);
-    grNotOr.link(2,3,4);
+    grNotOr.link("A","B",1.0);
+    grNotOr.link("B","C",2.0);
+    grNotOr.link("A","C",3.0);
+    grNotOr.link("A","D",5.0);
+    grNotOr.link("B","D",3.0);
+    grNotOr.link("B","C",4.0);
+  }
+
+  @Test
+  public void test_totDistance() {
+  	assertEquals(6.0,GraphUsage.totDistance(grNotOr));
   }
 
 
